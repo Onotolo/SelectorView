9cdeaf8013faca39c24d764970fc25c29d79da89
Onotolo <shiramhp@gmail.com>
2019-04-17 22:24:15 +0700

    Renaming core package

--- selectorview-core/src/main/java/my/onotolo/android/selector/utils/Extensions.kt
+++ selectorview-core/src/main/java/my/onotolo/android/selector/utils/Extensions.kt
@@ -2,7 +2,7 @@ package my.onotolo.android.selector.utils
 
 import android.view.View
 
-fun View.addSimpleOnLayoutChangeListener(listener: () -> Unit) {
+internal fun View.addSimpleOnLayoutChangeListener(listener: () -> Unit) {
     this.addOnLayoutChangeListener(object : View.OnLayoutChangeListener {
         override fun onLayoutChange(v: View?, left: Int, top: Int, right: Int, bottom: Int, oldLeft: Int, oldTop: Int, oldRight: Int, oldBottom: Int) {
             listener()
